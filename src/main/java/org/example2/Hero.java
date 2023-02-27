80af3c9dfd7bf60f7b1b81e881dd3decec9f8804
kazukiaidjudge@gmail.com <kazukiaidjudge@gmail.com>
2023-02-27 19:44:47 +0900

    add getter,setter,コンストラクタ

--- src/main/java/org/example2/Hero.java
+++ src/main/java/org/example2/Hero.java
@@ -1,12 +1,60 @@
 package org.example2;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Hero {
     private String name;
     private int hp,mp;
+
+    //name,hp,mpのgetter,setter
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setParameter(int hp,int mp){
+        this.hp = hp;
+        this.mp = mp;
+    }
+    public Map<String,Integer> getParameter(){
+
+        Map<String,Integer> parameter = new TreeMap<>();
+        parameter.put("hp",this.hp);
+        parameter.put("mp",this.mp);
+
+        return parameter;
+    }
+    public void setHp(){
+        this.hp = hp;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+    public void setMp(){
+        this.mp =mp;
+    }
+    public int getMp(){
+        return this.mp;
+    }
 
     //toStringメソッドのオーバーライド
     public String toString(){
         return "Hero（名前=" +this.name+
                 "HP="+this.hp+"MP="+this.mp+")";
+    }
+
+    //コンストラクタ
+    public Hero(String name,int hp,int mp){
+        setName(name);
+        setParameter(hp,mp);
+    }
+
+    //defaultコンストラクタ
+    public Hero(){
+        setName("None");
+        setParameter(0,0);
     }
 }
--- src/main/java/org/example2/Main.java
+++ src/main/java/org/example2/Main.java
@@ -2,7 +2,7 @@ package org.example2;
 
 public class Main {
     public static void main(String[] args){
-        Hero h = new Hero();
+        Hero h = new Hero("Man",10,10);
         System.out.println(h);
 
 
